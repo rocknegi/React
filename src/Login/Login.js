7b5664b49cfc636e6648b5246e5418021eb14795
rocknegi <rocknegi53@gmail.com>
2018-09-03 12:47:51 +0530

    remove left button from login page

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -38,6 +38,7 @@ class Login extends Component {
           <div className="Login">
             <AppBar
               title="Welcome To DockPlus"
+              showMenuIconButton={false}
             />
             <TextField
               hintText="Enter your Username"
